bd16f7b135d72864da3367b25d277499be981a58
Tyler Poland <poland.tyler@gmail.com>
2017-10-01 22:37:30 -0600

    Adds drop-shadow and rounded edges for location input views

---
+++ HtchHkr/View/RoundedShadowView.swift
@@ -0,0 +1,26 @@
+//
+//  RoundedShadowView.swift
+//  HtchHkr
+//
+//  Created by Tyler Poland on 10/1/17.
+//  Copyright © 2017 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowView: UIView {
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+
+    func setupView() {
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+        
+        self.layer.cornerRadius = 5.0
+    }
+
+}
