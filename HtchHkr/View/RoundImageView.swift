0646ef6dd4fe04792aab6605c0b3f8ff54914f74
Tyler Poland <poland.tyler@gmail.com>
2017-10-01 22:37:57 -0600

    Adds a rounded subview for the user image view

---
+++ HtchHkr/View/RoundImageView.swift
@@ -0,0 +1,22 @@
+//
+//  RoundImageView.swift
+//  HtchHkr
+//
+//  Created by Tyler Poland on 10/1/17.
+//  Copyright © 2017 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class RoundImageView: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+
+}
