39aba6658202f025570e8fdbd597102607388e88
Tyler Poland <poland.tyler@gmail.com>
2017-10-01 22:36:37 -0600

    Adds a background gradient view

---
+++ HtchHkr/View/GradientView.swift
@@ -0,0 +1,28 @@
+//
+//  GradientView.swift
+//  HtchHkr
+//
+//  Created by Tyler Poland on 10/1/17.
+//  Copyright © 2017 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        setupGradientView()
+    }
+    
+    func setupGradientView() {
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.init(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = CGPoint.zero
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.locations = [0.8, 1.0]
+        self.layer.addSublayer(gradient)
+    }
+
+}
