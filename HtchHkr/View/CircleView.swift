b2fa5ec526fd78d6c5d0b2acbc3fd2a87ea6e8f1
Tyler Poland <poland.tyler@gmail.com>
2017-10-01 22:36:28 -0600

    Adds a circular view for the location icon buttons

---
+++ HtchHkr/View/CircleView.swift
@@ -0,0 +1,29 @@
+//
+//  CircleView.swift
+//  HtchHkr
+//
+//  Created by Tyler Poland on 10/1/17.
+//  Copyright © 2017 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = borderColor?.cgColor
+    }
+    
+}
