8dfaba7352474c81c2074d389362b8a249fa6870
Tyler Poland <poland.tyler@gmail.com>
2017-10-04 20:41:00 -0600

    Login vc UI

--- HtchHkr/Controller/LeftSidePanelVC.swift
+++ HtchHkr/Controller/LeftSidePanelVC.swift
@@ -15,21 +15,11 @@ class LeftSidePanelVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUpLoginBtnPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
+        present(loginVC!, animated: true, completion: nil)
     }
-    */
 
 }
---
+++ HtchHkr/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  HtchHkr
+//
+//  Created by Tyler Poland on 10/4/17.
+//  Copyright © 2017 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeLoginPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
