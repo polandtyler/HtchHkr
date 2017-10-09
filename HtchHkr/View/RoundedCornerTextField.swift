b85dbf0998d4019463d999da0b68bacc86ab0ff3
Tyler Poland <poland.tyler@gmail.com>
2017-10-09 13:04:30 -0600

    Got roundedtext field working finally

---
+++ HtchHkr/View/RoundedCornerTextField.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedCornerTextField.swift
+//  HtchHkr
+//
+//  Created by Tyler Poland on 10/4/17.
+//  Copyright © 2017 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class RoundedCornerTextField: UITextField {
+    
+    var textRectOffset: CGFloat = 20
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+
+    func setupView() {
+        self.layer.cornerRadius = self.frame.height / 2
+        clipsToBounds = true
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: 0 + textRectOffset, y: 0 + (textRectOffset / 2), width: self.frame.width - textRectOffset, height: self.frame.height + textRectOffset)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: 0 + textRectOffset, y: 0 + (textRectOffset / 2), width: self.frame.width - textRectOffset, height: self.frame.height + textRectOffset)
+    }
+
+}
