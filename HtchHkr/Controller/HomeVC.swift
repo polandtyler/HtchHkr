e4f72eae200ecd63f23bba0477fddf99078c58aa
Tyler Poland <poland.tyler@gmail.com>
2017-10-10 05:57:24 -0600

    Location manager + creating map + getting user coordinates + centering map on coords

--- HtchHkr/Controller/HomeVC.swift
+++ HtchHkr/Controller/HomeVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import RevealingSplashView
+import CoreLocation
 
 class HomeVC: UIViewController, MKMapViewDelegate {
 
@@ -17,12 +18,25 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     
     var delegate: CenterVCDelegate?
     
+    var manager: CLLocationManager?
+    
+    var regionRadius: CLLocationDistance = 1000
+    
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+        checkLocationAuthStatus()
+        
         mapView.delegate = self
+        
+        centerMapOnUserLocation()
+        
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.heartBeat
         revealingSplashView.startAnimation()
@@ -36,7 +50,33 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     
     @IBAction func menuBtnPressed(_ sender: Any) {
         delegate?.toggleLeftPanel()
-        
+    }
+    
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        centerMapOnUserLocation()
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            manager?.startUpdatingLocation()
+        } else {
+            manager?.requestAlwaysAuthorization()
+        }
+    }
+    
+    func centerMapOnUserLocation() {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension HomeVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            checkLocationAuthStatus()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
     }
 }
 
