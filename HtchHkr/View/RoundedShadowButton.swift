020b9cba0264352c5cc1e0dc76c5390ef1460c28
Tyler Poland <poland.tyler@gmail.com>
2017-12-09 17:29:31 -0700

    cleanup

--- HtchHkr/View/RoundedShadowButton.swift
+++ HtchHkr/View/RoundedShadowButton.swift
@@ -43,7 +43,6 @@ class RoundedShadowButton: UIButton {
             }, completion: { (finished) in
                 if finished == true {
                     spinner.startAnimating()
-                    // spinner was oddly offset by 1px, hence the 1px correction below
                     spinner.center = CGPoint(x: self.frame.width / 2 + 1, y: self.frame.width / 2 + 1)
                     spinner.fadeTo(alphaValue: 1.0, withDuration: 0.2)
                 }
